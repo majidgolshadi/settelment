2f5a269b070dc48e3a7b1ba47140afb7dca20cd0
majidgolshadi <golshadi.majid@gmail.com>
2019-05-24 02:45:31 +0430

    handle null pointer

--- src/main/java/ir/carpino/settlement/repository/EntryTransactionRepository.java
+++ src/main/java/ir/carpino/settlement/repository/EntryTransactionRepository.java
@@ -3,11 +3,12 @@ package ir.carpino.settlement.repository;
 import ir.carpino.settlement.entity.mysql.EntityTransaction;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EntryTransactionRepository extends CrudRepository<EntityTransaction, String> {
 
-    @Query(value = "select * from entry_transaction where driver_id = ?1 order_by createdDate DESC limit 1", nativeQuery = true)
-    public EntityTransaction getDriverBalance(String driverId);
+    @Query(value = "SELECT * FROM entity_transaction WHERE driver_id=:driverId ORDER BY created_date DESC limit 1", nativeQuery = true)
+    public EntityTransaction getDriverBalance(@Param("driverId") String driverId);
 }
--- src/main/java/ir/carpino/settlement/service/WalletService.java
+++ src/main/java/ir/carpino/settlement/service/WalletService.java
@@ -2,27 +2,24 @@ package ir.carpino.settlement.service;
 
 import ir.carpino.settlement.entity.mysql.EntityTransaction;
 import ir.carpino.settlement.repository.EntryTransactionRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
-import java.util.List;
-
+@Slf4j
 @Service
 public class WalletService {
 
     @Autowired
     private EntryTransactionRepository entryTransactionRepo;
 
-    public HashMap<String, Long> getUsersBalance(List<String> userIds) {
-        HashMap<String, Long> result = new HashMap<>();
-        userIds.parallelStream().forEach(id ->result.put(id, getUserBalance(id)));
-
-        return result;
-    }
-
     public long getUserBalance(String userId) {
         EntityTransaction entityTransaction = entryTransactionRepo.getDriverBalance(userId);
-        return entityTransaction.getBalance();
+        if (entityTransaction != null) {
+            return entityTransaction.getBalance();
+        }
+
+        log.error(String.format("entry transaction balance for driver %s is null", userId));
+        return 0;
     }
 }

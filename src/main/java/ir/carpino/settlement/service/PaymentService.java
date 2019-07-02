81843df6ca58127fa9432ca8ffbef37b6bf85626
Majid Golshadi <golshadi.majid@gmail.com>
2019-07-02 16:33:22 +0430

    fix check shaba number length

--- src/main/java/ir/carpino/settlement/service/PaymentService.java
+++ src/main/java/ir/carpino/settlement/service/PaymentService.java
@@ -61,7 +61,7 @@ public class PaymentService {
         }
 
         // https://bmi.ir/fa/pages/192/%D9%85%D8%B4%D8%AE%D8%B5%D8%A7%D8%AA%20%D9%85%D9%84%DB%8C%20%D8%B4%D9%86%D8%A7%D8%B3%D9%87%20%D8%AD%D8%B3%D8%A7%D8%A8%20%D8%A8%D8%A7%D9%86%DA%A9%DB%8C%20%D8%A7%DB%8C%D8%B1%D8%A7%D9%86%20(%D8%B4%D8%A8%D8%A7)
-        if (driver.getBankAccountInfo().getShabaNumber().length() > 24) {
+        if (driver.getBankAccountInfo().getShabaNumber().length() < 24) {
             log.warn(String.format("driver %s invalid shaba number", driver.getId()));
             return false;
         }

451997fef864c01a0958ff2a15a5b35f5b4a769c
majidgolshadi <golshadi.majid@gmail.com>
2019-07-06 23:30:43 +0430

    add driverControllerTest file

---
+++ src/test/java/ir/carpino/settlement/controller/DriverControllerTests.java
@@ -0,0 +1,11 @@
+package ir.carpino.settlement.controller;
+
+import org.junit.Test;
+
+public class DriverControllerTests {
+
+    @Test
+    public void readCsvFile_FileNotFoundError_Test() {
+
+    }
+}
